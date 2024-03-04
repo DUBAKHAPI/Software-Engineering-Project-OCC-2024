c0b2edc4734bb932e7312c39a866156af5b54979
sminhas1 <shabeg.minhas@gmail.com>
2024-03-04 15:45:51 -0500

    shabeg - GUI buttons start

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# This is a sample Python script.
+###PROPERTY OF
+###SHABEG, JOSE, NATHAN, MARSELIS, RASHA
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import tkinter as tk
 from tkinter import Button, Entry, Label, messagebox, Toplevel
